d30563c25cd5577006e86ae6a304a8249908ecbc
Dimitry Kislichenko <dimitrykislichenko@Dimitrys-iMac.local>
2018-11-15 16:05:07 +0200

    Rewrite tests for Dice contract

--- test/Dice.js
+++ test/Dice.js
@@ -10,11 +10,16 @@ contract('Dice', accounts => {
 
     let diceInstance;
 
+    let reset = true;
     beforeEach(async () => {
-        diceInstance = await Dice.new();
+        if (reset) {
+            diceInstance = await Dice.new();
+        }
+
+        reset = true;
     });
 
-    describe('token attributes', () => {
+    describe('game attributes', () => {
         it('has the address', () => {
             const address = diceInstance.address;
             assert.notEqual(address, '0x0', 'has contract address');
@@ -161,188 +166,200 @@ contract('Dice', accounts => {
         });
     });
 
-    // it('withdraw funds', async () => {
-    //     const withdrawAddress = accounts[2];
-    //     const withdrawAddressBalance = await web3.eth.getBalance(
-    //         withdrawAddress,
-    //     );
-    //     const jackpotSize = await diceInstance.jackpotSize();
+    describe('withdraw funds', () => {
+        it('should fail to withdraw more then current balance', async () => {
+            const withdrawAddress = accounts[2];
 
-    //     // Try to withdraw more then available balance
-    //     await diceInstance
-    //         .withdrawFunds(withdrawAddress, web3.toWei(100))
-    //         .then(assert.fail)
-    //         .catch(e => {
-    //             assert(
-    //                 e.message.indexOf('revert') >= 0,
-    //                 "can't withdraw more then current balance",
-    //             );
-    //         });
+            // Try to withdraw more then available balance
+            await diceInstance
+                .withdrawFunds(withdrawAddress, 1)
+                .then(assert.fail)
+                .catch(e => {
+                    assert(
+                        e.message.indexOf('revert') >= 0,
+                        "can't withdraw more then current balance",
+                    );
+                });
+        });
 
-    //     // Increase balance and remember final amount
-    //     const contractBalance = await diceInstance
-    //         .send(web3.toWei(1, 'ether'), { from: owner })
-    //         .then(() => {
-    //             return web3.eth.getBalance(diceInstance.address);
-    //         });
+        it('should fail to withdraw amount locked in jackpot', async () => {
+            // Send 1 ether on balance
+            await diceInstance.send(web3.toWei(1, 'ether'), { from: owner });
 
-    //     // Should fail withdraw because of locked jackpot
-    //     await diceInstance
-    //         .withdrawFunds(withdrawAddress, contractBalance.toNumber())
-    //         .then(assert.fail)
-    //         .catch(e => {
-    //             assert(
-    //                 e.message.indexOf('revert') >= 0,
-    //                 "can't withdraw locked jackpot size",
-    //             );
-    //         });
+            // Send 1 ether on jacpot
+            await diceInstance.increaseJackpot(web3.toWei(1, 'ether'), {
+                from: owner,
+            });
 
-    //     // Should process withdraw correctly
-    //     await diceInstance
-    //         .withdrawFunds(
-    //             withdrawAddress,
-    //             contractBalance.toNumber() -
-    //                 web3.toWei(jackpotSize.toNumber(), 'ether'),
-    //         )
-    //         .then(() => {
-    //             return web3.eth.getBalance(diceInstance.address);
-    //         })
-    //         .then(amount => {
-    //             assert.equal(
-    //                 amount.toNumber(),
-    //                 web3.toWei(jackpotSize.toNumber(), 'ether'),
-    //                 'has withdraw all available founds',
-    //             );
-    //             return web3.eth.getBalance(withdrawAddress);
-    //         })
-    //         .then(amount => {
-    //             const expectedBalance =
-    //                 withdrawAddressBalance.toNumber() +
-    //                 (contractBalance.toNumber() -
-    //                     web3.toWei(jackpotSize.toNumber(), 'ether'));
+            // Should fail withdraw because of locked jackpot
+            await diceInstance
+                .withdrawFunds(owner, web3.toWei(1, 'ether'))
+                .then(assert.fail)
+                .catch(e => {
+                    assert(
+                        e.message.indexOf('revert') >= 0,
+                        "can't withdraw locked jackpot size",
+                    );
+                });
+        });
 
-    //             assert.equal(
-    //                 amount.toNumber(),
-    //                 expectedBalance,
-    //                 'withdraw address balance has increased',
-    //             );
-    //         });
-    // });
+        it('should withdraw funds correctly', async () => {
+            // Send 1 ether on balance
+            await diceInstance.send(web3.toWei(1, 'ether'), { from: owner });
 
-    // it('place bet', async () => {
-    //     const commitLastBlock = web3.eth.blockNumber + 200;
-    //     const packedCommit = '0x' + lodash.padStart((1).toString(16), 64, 0);
-    //     const commit = web3.sha3(packedCommit, {
-    //         encoding: 'hex',
-    //     });
+            // Remember balance of sender
+            const ownerBalanceBefore = await web3.eth.getBalance(owner);
 
-    //     const packed = [
-    //         '0x',
-    //         lodash.padStart(commitLastBlock.toString(16), 10, 0),
-    //         commit.substr(2),
-    //     ].join('');
+            // Withdraw 1 ether from balance
+            await diceInstance.withdrawFunds(owner, web3.toWei(1, 'ether'));
 
-    //     const hash = web3.sha3(packed, {
-    //         encoding: 'hex',
-    //     });
+            // Make sure that contract balance is 0
+            const diceBalance = await web3.eth.getBalance(diceInstance.address);
+            assert.equal(
+                diceBalance.toNumber(),
+                0,
+                'has withdrawen all available founds',
+            );
 
-    //     const commitSignature = web3.eth.sign(secretSigner, hash);
+            // Get current balance of sender
+            const ownerBalanceAfter = await web3.eth.getBalance(owner);
+            assert.isTrue(
+                ownerBalanceAfter.toNumber() > ownerBalanceBefore.toNumber(),
+                'funds should be returned to specified address',
+            );
+        });
+    });
 
-    //     const r = '0x' + commitSignature.substr(2, 64);
-    //     const s = '0x' + commitSignature.substr(66, 64);
-    //     const v = parseInt(commitSignature.substr(130, 2)) + 27;
+    describe('bet', () => {
+        beforeEach(async () => {
+            reset = false;
 
-    //     // Send some ether to the contract
-    //     await diceInstance.send(web3.toWei(3, 'ether'), { from: owner });
+            // Set secret signer address
+            await diceInstance.setSecretSigner(secretSigner);
 
-    //     await diceInstance
-    //         .placeBet(1, 6, commitLastBlock, commit, v, r, s, {
-    //             from: gambler,
-    //             value: web3.toWei(0.1, 'ether'),
-    //         })
-    //         .then(receipt => {
-    //             assert.equal(receipt.logs.length, 1, 'triggers one event');
-    //             assert.equal(
-    //                 receipt.logs[0].event,
-    //                 'BetPlaced',
-    //                 'should be "BetPlaced" event',
-    //             );
-    //             assert.equal(
-    //                 receipt.logs[0].args.gambler,
-    //                 gambler,
-    //                 'logs the account the bet is placed from',
-    //             );
-    //             assert.equal(
-    //                 receipt.logs[0].args.amount.toNumber(),
-    //                 web3.toWei(0.1),
-    //                 'logs the amount placed on the bet',
-    //             );
-    //             assert.equal(
-    //                 receipt.logs[0].args.betMask.toNumber(),
-    //                 1,
-    //                 'logs the bet mask',
-    //             );
-    //             assert.equal(
-    //                 receipt.logs[0].args.modulo.toNumber(),
-    //                 6,
-    //                 'logs the modulo',
-    //             );
-    //         });
-    // });
+            // Set max profit value
+            await diceInstance.setMaxProfit(web3.toWei(200000, 'ether'));
+        });
 
-    // it('settle bet', async () => {
-    //     const commit = 1;
-    //     await diceInstance.settleBet(commit, 0).then(receipt => {
-    //         assert.equal(receipt.logs.length, 1, 'triggers one event');
-    //         assert.equal(
-    //             receipt.logs[0].event,
-    //             'Payment',
-    //             'should be "Payment" event',
-    //         );
-    //         assert.equal(
-    //             receipt.logs[0].args.beneficiary,
-    //             gambler,
-    //             'logs the beneficiary address the payment is transfered to',
-    //         );
-    //     });
-    // });
+        it('should place a bet', async () => {
+            const commitLastBlock = web3.eth.blockNumber + 200;
+            const packedCommit =
+                '0x' + lodash.padStart((1).toString(16), 64, 0);
+            const commit = web3.sha3(packedCommit, {
+                encoding: 'hex',
+            });
 
-    // // it('refund bet', () => {});
-    // // it('clear storage', () => {});
+            const packed = [
+                '0x',
+                lodash.padStart(commitLastBlock.toString(16), 10, 0),
+                commit.substr(2),
+            ].join('');
 
-    // it('kill contract', async () => {
-    //     await diceInstance
-    //         .kill({ from: nextOwner })
-    //         .then(assert.fail)
-    //         .catch(e => {
-    //             assert(
-    //                 e.message.indexOf('revert') >= 0,
-    //                 'must be owner to kill contract',
-    //             );
-    //         });
+            const hash = web3.sha3(packed, {
+                encoding: 'hex',
+            });
 
-    //     const ownerBalance = await web3.eth.getBalance(owner);
-    //     const contractBalance = await web3.eth.getBalance(diceInstance.address);
+            const commitSignature = web3.eth.sign(secretSigner, hash);
 
-    //     diceInstance
-    //         .kill({ from: owner })
-    //         .then(() => {
-    //             return web3.eth.getBalance(diceInstance.address);
-    //         })
-    //         .then(balance => {
-    //             assert.equal(
-    //                 balance.toNumber(),
-    //                 0,
-    //                 'has to withdraw all funds',
-    //             );
-    //             return web3.eth.getBalance(owner);
-    //         })
-    //         .then(balance => {
-    //             assert.equal(
-    //                 ownerBalance.toNumber() + contractBalance.toNumber(),
-    //                 balance.toNumber(),
-    //                 'all funds from contract should be transfered to owner address',
-    //             );
-    //         });
-    // });
+            const r = '0x' + commitSignature.substr(2, 64);
+            const s = '0x' + commitSignature.substr(66, 64);
+            const v = parseInt(commitSignature.substr(130, 2)) + 27;
+
+            // Send some ether to the contract
+            await diceInstance.send(web3.toWei(1, 'ether'), { from: owner });
+
+            await diceInstance
+                .placeBet(1, 6, commitLastBlock, commit, v, r, s, {
+                    from: gambler,
+                    value: web3.toWei(0.1, 'ether'),
+                })
+                .then(receipt => {
+                    assert.equal(receipt.logs.length, 1, 'triggers one event');
+                    assert.equal(
+                        receipt.logs[0].event,
+                        'BetPlaced',
+                        'should be "BetPlaced" event',
+                    );
+                    assert.equal(
+                        receipt.logs[0].args.gambler,
+                        gambler,
+                        'logs the account the bet is placed from',
+                    );
+                    assert.equal(
+                        receipt.logs[0].args.amount.toNumber(),
+                        web3.toWei(0.1),
+                        'logs the amount placed on the bet',
+                    );
+                    assert.equal(
+                        receipt.logs[0].args.betMask.toNumber(),
+                        1,
+                        'logs the bet mask',
+                    );
+                    assert.equal(
+                        receipt.logs[0].args.modulo.toNumber(),
+                        6,
+                        'logs the modulo',
+                    );
+                });
+        });
+
+        it('settle bet', async () => {
+            const commit = 1;
+            await diceInstance.settleBet(commit, 0).then(receipt => {
+                assert.equal(receipt.logs.length, 1, 'triggers one event');
+                assert.equal(
+                    receipt.logs[0].event,
+                    'Payment',
+                    'should be "Payment" event',
+                );
+                assert.equal(
+                    receipt.logs[0].args.beneficiary,
+                    gambler,
+                    'logs the beneficiary address the payment is transfered to',
+                );
+            });
+        });
+    });
+
+    describe('destroy contract', () => {
+        it('should fail if called not by owner', async () => {
+            await diceInstance
+                .kill({ from: nextOwner })
+                .then(assert.fail)
+                .catch(e => {
+                    assert(
+                        e.message.indexOf('revert') >= 0,
+                        'must be owner to kill contract',
+                    );
+                });
+        });
+
+        it('should successfully destroy the contract', async () => {
+            const ownerBalance = await web3.eth.getBalance(owner);
+            const contractBalance = await web3.eth.getBalance(
+                diceInstance.address,
+            );
+
+            diceInstance
+                .kill({ from: owner })
+                .then(() => {
+                    return web3.eth.getBalance(diceInstance.address);
+                })
+                .then(balance => {
+                    assert.equal(
+                        balance.toNumber(),
+                        0,
+                        'has to withdraw all funds',
+                    );
+                    return web3.eth.getBalance(owner);
+                })
+                .then(balance => {
+                    assert.equal(
+                        ownerBalance.toNumber() + contractBalance.toNumber(),
+                        balance.toNumber(),
+                        'all funds from contract should be transfered to owner address',
+                    );
+                });
+        });
+    });
 });
