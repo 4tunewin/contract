5d305b3b3d247a2e4066f3285fc3ec5aed6f37c5
Dimitry Kislichenko <dimitrykislichenko@Dimitrys-iMac.local>
2018-12-17 13:07:46 +0200

    Add ledger wallet as provider for mainnet

--- truffle.js
+++ truffle.js
@@ -1,4 +1,5 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
+const LedgerWalletProvider = require('truffle-ledger-provider');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -7,11 +8,15 @@ module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration>
     // to customize your Truffle configuration!
     networks: {
+        // Local development network
+        // https://truffleframework.com/ganache
         development: {
             host: '127.0.0.1',
             port: 8545,
             network_id: '*', // match all networks id
         },
+        // Proof-of-authority test network.
+        // https://www.rinkeby.io/
         rinkeby: {
             provider: () => {
                 return new HDWalletProvider(
@@ -23,6 +28,25 @@ module.exports = {
             },
             gasPrice: 25000000000,
             network_id: 4,
+        },
+        // Main ethereum network
+        production: {
+            provider: () => {
+                return new LedgerWalletProvider(
+                    {
+                        networkId: 1,
+                        path: "44'/60'/0'/0",
+                        askConfirm: false,
+                        accountsLength: 1,
+                        accountsOffset: 0,
+                    },
+                    `https://rinkeby.infura.io/v3/${
+                        process.env.INFURA_API_KEY
+                    }`,
+                );
+            },
+            gasPrice: 25000000000,
+            network_id: 1,
         },
     },
     solc: {
